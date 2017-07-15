43b6c35d21404070dea38792558397c3aa157aa1
Steve Schofield <stevebschofield@gmail.com>
2017-07-15 15:50:11 +0200

    creds

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_Description ="henri_demo"
+Security_Profile_ID  = "amzn1.application.6de3014e37b34c9688472016b1a10efb"
+Client_ID = "amzn1.application-oa2-client.f6fce03b085e4626ba09437ec26ecf36"
+Client_Secret = "f3375598f224edd3433ca9d7ab1bdd2909bc3f35f660297e5de2c645cf28d0b6"
+Product_ID = "Henri"
 
 #Redis
-redis_url = ""
+redis_url = "redis://h:p6d7c6c169e4218198bef75ae80c1326e2bdad4d4c579bf3f4543b8ef9df824ce@ec2-34-230-117-175.compute-1.amazonaws.com:37569"
